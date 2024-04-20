36ce044cae3ee1b08a4ef74f3920dcaa1421c561
Matteo <43893929+MatteoDeFaria@users.noreply.github.com>
2024-04-20 20:32:58 +0200

    [update]: background color dark mode page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Case from '@/components/Case';
 export default function HomePage() {
   return (
     <main className=''>
-      <div className=''>
+      <div className='bg-white dark:bg-gray-800'>
         <Case />
       </div>
     </main>
